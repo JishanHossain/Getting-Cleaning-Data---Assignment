a63f2a4fbaee9720eb36cb66681c60a0a940c036
JishanHossain <jishanihossain@gmail.com>
2017-03-04 10:09:50 +1100

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,6 +1,6 @@
 # Getting and cleaning data - assignment
 # Set working directory 
-setwd("C:/My Documents/1 Jishan/2 R (Data Science)/Data Scientists (John Hopkins) Coursera/3 Getting & Cleaning Data/Assignment/My Project Final")
+setwd("~./My Project Final")
 library(dplyr)
 if (!file.exists("./data")) {
     dir.create("./data")
@@ -26,12 +26,12 @@ X_train <- read.table("./data/UCI HAR Dataset/train/X_train.txt")
 Y_train <- read.table("./data/UCI HAR Dataset/train/Y_train.txt")
 Sub_train <- read.table("./data/UCI HAR Dataset/train/subject_train.txt")
 
-# 1. Merging the 'training' and the 'test' sets to create one data set
+# 1. Merging the 'training' and the 'test' sets to create one data sets
 X_total <- rbind(X_train, X_test)
 Y_total <- rbind(Y_train, Y_test)
 Sub_total <- rbind(Sub_train, Sub_test)
 
-# 2. Extracts only the measurements on the 'mean' and 'standard deviation' for each 'measurement'.
+# 2. Extracts only the measurements on the 'mean' and 'standard deviation' for each 'measurement'
 selected_var <- variable_names[grep("mean\\(\\)|std\\(\\)",variable_names[,2]),]
 X_total <- X_total[,selected_var[,1]]
 
@@ -43,8 +43,7 @@ activitylabel <- Y_total[,-1]
 # 4. Appropriately labels the data set with descriptive variable names
 colnames(X_total) <- variable_names[selected_var[,1],2]
 
-# 5. From the data set in step 4, creates a second, independent tidy data set with the average 
-# of each variable for each activity and each subject
+# 5. Creates a second, independent tidy data set with the average of each variable for each activity and each subject
 colnames(Sub_total) <- "subject"
 total <- cbind(X_total, activitylabel, Sub_total)
 total_avg <- total %>% group_by(activitylabel, subject) %>% summarize_each(funs(mean))
